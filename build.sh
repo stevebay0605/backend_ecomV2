fe03c33b4b74602a743d69a688d0d6037013370d
stevebay0605 <bayonnestevekelly@gmail.com>
2025-10-14 19:48:45 +0100

    Add custom superuser creation

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser if it doesn't exist
+# Change 'admin', 'admin@example.com', 'adminpass123' with your own values
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='stevebay').exists() or User.objects.create_superuser('stevebay', 'bayonnestevekelly@gmail.com', 'jujub2b242*') | python manage.py shell
