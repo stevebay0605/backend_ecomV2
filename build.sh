9be3a759862a6fb1b8a4d3210489ea2e892e6eca
stevebay0605 <bayonnestevekelly@gmail.com>
2025-10-14 19:51:55 +0100

    correction de syntaxe

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ python manage.py migrate
 
 # Create superuser if it doesn't exist
 # Change 'admin', 'admin@example.com', 'adminpass123' with your own values
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='stevebay').exists() or User.objects.create_superuser('stevebay', 'bayonnestevekelly@gmail.com', 'jujub2b242*') | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='stevebay').exists() or User.objects.create_superuser('stevebay', 'bayonnestevekelly@gmail.com', 'jujub2b242*')" | python manage.py shell
